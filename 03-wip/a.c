72dacaa676881c8f283ef33ca2d1e0923d69bcbe
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2024-12-04 11:56:51 -0800

    Clean up 03a (mostly naming)

--- 03-wip/a.c
+++ 03-wip/a.c
@@ -4,23 +4,36 @@
 #define TRUE 1
 #define FALSE 0
 
-const int STATE_EXPECT_M = 10;
-const int STATE_EXPECT_U = 20;
-const int STATE_EXPECT_L = 30;
-const int STATE_EXPECT_OPEN_PAREN = 40;
-const int STATE_EXPECT_LEFT_OPERAND = 50;
-const int STATE_EXPECT_COMMA = 60;
-const int STATE_EXPECT_RIGHT_OPERAND = 70;
-const int STATE_EXPECT_CLOSE_PAREN = 80;
-
 typedef struct state {
-  int kind;
+  int kind; // Should be one of EXPECT_*
   int left_operand;
   int right_operand;
 } state;
 
-const state INITIAL_STATE = { .kind = STATE_EXPECT_M, .left_operand = 0, .right_operand = 0 };
-const state SECOND_STATE = { .kind = STATE_EXPECT_U, .left_operand = 0, .right_operand = 0 };
+const int EXPECT_M = 10;
+const int EXPECT_U = 20;
+const int EXPECT_L = 30;
+const int EXPECT_OPEN_PAREN = 40;
+const int EXPECT_LEFT_OPERAND = 50;
+const int EXPECT_COMMA = 60;
+const int EXPECT_RIGHT_OPERAND = 70;
+const int EXPECT_CLOSE_PAREN = 80;
+
+const state INITIAL_STATE = { .kind = EXPECT_M, .left_operand = 0, .right_operand = 0 };
+const state SECOND_STATE = { .kind = EXPECT_U, .left_operand = 0, .right_operand = 0 };
+
+state next_state(state current, int ch, int* answer);
+
+int main() {
+  FILE *file = fopen("in", "r");
+  state my_state = INITIAL_STATE;
+  int ch;
+  int answer = 0;
+  while ((ch = getc(file)) != EOF) {
+    my_state = next_state(my_state, ch, &answer);
+  }
+  printf("%d\n", answer);
+}
 
 int is_int(int ch) {
   switch (ch) {
@@ -46,82 +59,82 @@ state advance_state(state current, int kind) {
   return result;
 }
 
-int ctoi(int ch) {
+int char_to_int(int ch) {
   return ch - '0';
 }
 
 state next_state(state current, int ch, int* answer) {
   switch (current.kind) {
-    case STATE_EXPECT_M:
+    case EXPECT_M:
       if (ch == 'm') {
-        return advance_state(current, STATE_EXPECT_U);
+        return advance_state(current, EXPECT_U);
       } else {
         return current;
       }
-    case STATE_EXPECT_U:
+    case EXPECT_U:
       if (ch == 'u') {
-        return advance_state(current, STATE_EXPECT_L);
+        return advance_state(current, EXPECT_L);
       } else if (ch == 'm') {
         return SECOND_STATE;
       } else {
         return INITIAL_STATE;
       }
-    case STATE_EXPECT_L:
+    case EXPECT_L:
       if (ch == 'l') {
-        return advance_state(current, STATE_EXPECT_OPEN_PAREN);
+        return advance_state(current, EXPECT_OPEN_PAREN);
       } else if (ch == 'm') {
         return SECOND_STATE;
       } else {
         return INITIAL_STATE;
       }
-    case STATE_EXPECT_OPEN_PAREN:
+    case EXPECT_OPEN_PAREN:
       if (ch == '(') {
-        return advance_state(current, STATE_EXPECT_LEFT_OPERAND);
+        return advance_state(current, EXPECT_LEFT_OPERAND);
       } else if (ch == 'm') {
         return SECOND_STATE;
       } else {
         return INITIAL_STATE;
       }
-    case STATE_EXPECT_LEFT_OPERAND:
+    case EXPECT_LEFT_OPERAND:
       if (is_int(ch)) {
-        state result = advance_state(current, STATE_EXPECT_COMMA);
-        result.left_operand = ctoi(ch);
+        state result = advance_state(current, EXPECT_COMMA);
+        result.left_operand = char_to_int(ch);
         return result;
       } else if (ch == 'm') {
         return SECOND_STATE;
       } else {
         return INITIAL_STATE;
       }
-    case STATE_EXPECT_COMMA:
+    case EXPECT_COMMA:
       if (is_int(ch)) {
         state result = current;
-        result.kind = STATE_EXPECT_COMMA;
+        result.kind = EXPECT_COMMA;
         result.left_operand *= 10;
-        result.left_operand += ctoi(ch);
+        result.left_operand += char_to_int(ch);
         return result;
       } else if (ch == ',') {
-        return advance_state(current, STATE_EXPECT_RIGHT_OPERAND);
+        return advance_state(current, EXPECT_RIGHT_OPERAND);
       } else if (ch == 'm') {
         return SECOND_STATE;
       } else {
         return INITIAL_STATE;
       }
-    case STATE_EXPECT_RIGHT_OPERAND:
+    case EXPECT_RIGHT_OPERAND:
       if (is_int(ch)) {
-        state result = advance_state(current, STATE_EXPECT_CLOSE_PAREN);
-        result.right_operand = ctoi(ch);
+        state result = advance_state(current, EXPECT_CLOSE_PAREN);
+        result.right_operand = char_to_int(ch);
         return result;
       } else if (ch == 'm') {
         return SECOND_STATE;
       } else {
         return INITIAL_STATE;
       }
-    case STATE_EXPECT_CLOSE_PAREN:
+    case EXPECT_CLOSE_PAREN:
       if (is_int(ch)) {
         state result = current;
-        result.kind = STATE_EXPECT_CLOSE_PAREN;
+        result.kind = EXPECT_CLOSE_PAREN;
         result.right_operand *= 10;
-        result.right_operand += ctoi(ch);
+        result.right_operand += char_to_int(ch);
         return result;
       } else if (ch == 'm') {
         return SECOND_STATE;
@@ -135,15 +148,4 @@ state next_state(state current, int ch, int* answer) {
       fprintf(stderr, "Invalid input to next_state()\n");
       exit(EXIT_FAILURE);
   }
-}
-
-int main() {
-  FILE *file = fopen("in", "r");
-  state my_state = INITIAL_STATE;
-  int ch;
-  int answer = 0;
-  while ((ch = getc(file)) != EOF) {
-    my_state = next_state(my_state, ch, &answer);
-  }
-  printf("%d\n", answer);
 }
