ffaf6ee8af0d508f6bb5b45af4641ef9e99d4c3f
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2024-12-05 22:35:26 -0800

    Solve 06a

---
+++ 06/a.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+// SIZE = the width of the input = the height of the input
+const int SIZE = 10;
+
+// The example input with newlines removed
+char input[] = "....#..............#............#..............#.............#..^.............#.#...............#...";
+
+
+typedef struct Vector {
+  int r;
+  int c;
+} Vector;
+
+const Vector UP = { -1, 0 };
+const Vector DOWN = { 1, 0 };
+const Vector LEFT = { 0, -1 };
+const Vector RIGHT = { 0, 1 };
+
+
+char get_char(Vector pos) {
+  return input[pos.r * SIZE + pos.c];
+}
+
+void set_char(Vector pos, char new_value) {
+  input[pos.r * SIZE + pos.c] = new_value;
+}
+
+bool in_bounds(Vector pos) {
+  return (
+    0 <= pos.r &&
+    pos.r < SIZE &&
+    0 <= pos.c &&
+    pos.c < SIZE
+  );
+}
+
+Vector add(Vector v1, Vector v2) {
+  return (Vector){
+    .r = v1.r + v2.r,
+    .c = v1.c + v2.c,
+  };
+}
+
+Vector turn(Vector direction) {
+  if (direction.r == UP.r && direction.c == UP.c) {
+    return RIGHT;
+  } else if (direction.r == RIGHT.r && direction.c == RIGHT.c) {
+    return DOWN;
+  } else if (direction.r == DOWN.r && direction.c == DOWN.c) {
+    return LEFT;
+  } else if (direction.r == LEFT.r && direction.c == LEFT.c) {
+    return UP;
+  } else {
+    fprintf(stderr, "Unexpected direction\n");
+    exit(EXIT_FAILURE);
+  }
+}
+
+Vector find_start() {
+  Vector result;
+  for (int r = 0; r < SIZE; r++) {
+    for (int c = 0; c < SIZE; c++) {
+      Vector pos = { r, c };
+      if (get_char(pos) == '^') {
+        result = pos;
+      }
+    }
+  }
+  return result;
+}
+
+int main() {
+  Vector pos = find_start();
+  Vector direction = UP;
+
+  // Simulate the guard's movement
+  while (in_bounds(pos)) {
+    set_char(pos, 'X');
+    Vector next = add(pos, direction);
+    if (in_bounds(next) && get_char(next) == '#') {
+      direction = turn(direction);
+    } else {
+      pos = next;
+    }
+  }
+
+  // Count visited positions
+  char* ch = input;
+  int answer = 0;
+  while (*ch != 0) {
+    if (*ch == 'X') {
+      answer++;
+    }
+    ch++;
+  }
+  printf("%d\n", answer);
+}
