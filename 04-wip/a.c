482be64e255ae8a5860a7a42d72b4ba22d45b268
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2024-12-06 10:44:36 -0800

    04a wip: "Done" but incorrect answer

--- 04-wip/a.c
+++ 04-wip/a.c
@@ -1,68 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 /* fprintf(stderr, "Input too long\n"); */
 /* exit(EXIT_FAILURE); */
 
 const int PATTERN_SIZE = 4;
-const int PATTERN[PATTERN_SIZE] = { 'A', 'B', 'C', 'D', };
-
-typedef struct Grid {
-  int* arr;
-  int row_count;
-  int col_count;
-} Grid;
+const char PATTERN[PATTERN_SIZE] = { 'X', 'M', 'A', 'S', };
 
 typedef struct Vector {
   int r;
   int c;
 } Vector;
 
-const Vector NORTHWEST = { .r = -1, .c = -1 };
-const Vector NORTHEAST = { .r = -1, .c = 1 };
-const Vector SOUTHWEST = { .r = 1, .c = -1 };
-const Vector SOUTHEAST = { .r = 1, .c = 1 };
+// UDLR or NSEW?
 
-/* const int example_arr[] = { */
-/*   'M', 'M', 'M', 'S', 'X', 'X', 'M', 'A', 'S', 'M', */
-/*   'M', 'S', 'A', 'M', 'X', 'M', 'S', 'M', 'S', 'A', */
-/*   'A', 'M', 'X', 'S', 'X', 'M', 'A', 'A', 'M', 'M', */
-/*   'M', 'S', 'A', 'M', 'A', 'S', 'M', 'S', 'M', 'X', */
-/*   'X', 'M', 'A', 'S', 'A', 'M', 'X', 'A', 'M', 'M', */
-/*   'X', 'X', 'A', 'M', 'M', 'X', 'X', 'A', 'M', 'A', */
-/*   'S', 'M', 'S', 'M', 'S', 'A', 'S', 'X', 'S', 'S', */
-/*   'S', 'A', 'X', 'A', 'M', 'A', 'S', 'A', 'A', 'A', */
-/*   'M', 'A', 'M', 'M', 'M', 'X', 'M', 'M', 'M', 'M', */
-/*   'M', 'X', 'M', 'X', 'A', 'X', 'M', 'A', 'S', 'X', */
+const Vector UP = { -1, 0 };
+const Vector DOWN = { 1, 0 };
+const Vector LEFT = { 0, -1 };
+const Vector RIGHT = { 0, 1 };
+
+const Vector NORTHWEST = { -1, -1 };
+const Vector NORTHEAST = { -1, 1 };
+const Vector SOUTHWEST = { 1, -1 };
+const Vector SOUTHEAST = { 1, 1 };
+
+const int SIZE = 10;
+
+const char INPUT[SIZE * SIZE] = {
+  'M', 'M', 'M', 'S', 'X', 'X', 'M', 'A', 'S', 'M',
+  'M', 'S', 'A', 'M', 'X', 'M', 'S', 'M', 'S', 'A',
+  'A', 'M', 'X', 'S', 'X', 'M', 'A', 'A', 'M', 'M',
+  'M', 'S', 'A', 'M', 'A', 'S', 'M', 'S', 'M', 'X',
+  'X', 'M', 'A', 'S', 'A', 'M', 'X', 'A', 'M', 'M',
+  'X', 'X', 'A', 'M', 'M', 'X', 'X', 'A', 'M', 'A',
+  'S', 'M', 'S', 'M', 'S', 'A', 'S', 'X', 'S', 'S',
+  'S', 'A', 'X', 'A', 'M', 'A', 'S', 'A', 'A', 'A',
+  'M', 'A', 'M', 'M', 'M', 'X', 'M', 'M', 'M', 'M',
+  'M', 'X', 'M', 'X', 'A', 'X', 'M', 'A', 'S', 'X',
+};
+
+/* const char INPUT[SIZE * SIZE] = { */
+/*   'X', 'M', 'A', 'S', '.', '.', 'X', 'M', 'A', 'S', */
+/*   '.', '.', '.', '.', '.', 'X', 'M', 'A', 'S', '.', */
+/*   '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', */
+/*   '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', */
+/*   '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', */
+/*   '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', */
+/*   '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', */
+/*   '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', */
+/*   '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', */
+/*   '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', */
 /* }; */
 
-const int example_arr[] = {
-  '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a',
-  't', '.', '.', '.', '.', '.', '.', '.', '.', 'b',
-  's', '.', '.', '.', '.', '.', '.', '.', '.', 'c',
-  'n', '.', '.', '.', '.', '.', '.', '.', '.', 'd',
-  'q', '.', '.', '.', '.', '.', '.', '.', '.', 'e',
-  'p', '.', '.', '.', '.', '.', '.', '.', '.', 'f',
-  'o', '.', '.', '.', '.', '.', '.', '.', '.', 'g',
-  'n', '.', '.', '.', '.', '.', '.', '.', '.', 'h',
-  'm', '.', '.', '.', '.', '.', '.', '.', '.', 'i',
-  'l', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A',
-};
-
-const Grid example_grid = {
-  .row_count = 10,
-  .col_count = 10,
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wincompatible-pointer-types-discards-qualifiers"
-  .arr = example_arr,
-#pragma GCC diagnostic pop
-};
-
-/* Vector* a = (Vector*) malloc(sizeof(Vector) * 10); */
-
-/* const Vector a[] = { */
-/*   (Vector){ 0, 0 }, */
-/*   (Vector){ 0, 1 }, */
+/* const char INPUT[SIZE * SIZE] = { */
+/*   '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', */
+/*   't', '.', '.', '.', '.', '.', '.', '.', '.', 'b', */
+/*   's', '.', '.', '.', '.', '.', '.', '.', '.', 'c', */
+/*   'n', '.', '.', '.', '.', '.', '.', '.', '.', 'd', */
+/*   'q', '.', '.', '.', '.', '.', '.', '.', '.', 'e', */
+/*   'p', '.', '.', '.', '.', '.', '.', '.', '.', 'f', */
+/*   'o', '.', '.', '.', '.', '.', '.', '.', '.', 'g', */
+/*   'n', '.', '.', '.', '.', '.', '.', '.', '.', 'h', */
+/*   'm', '.', '.', '.', '.', '.', '.', '.', '.', 'i', */
+/*   'l', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A', */
 /* }; */
 
 void print_int(int n) {
@@ -70,84 +71,82 @@ void print_int(int n) {
 }
 
 // todo: Try using a macro maybe
-int get_grid_item(Grid grid, int r, int c) {
-  return grid.arr[r * grid.col_count + c];
+char get_input_char(Vector v) {
+  return INPUT[v.r * SIZE + v.c];
 }
 
-int in_bounds(Grid grid, int r, int c) {
+int in_bounds(Vector v) {
   return (
-    0 <= r &&
-    r < grid.row_count &&
-    0 <= c &&
-    c < grid.col_count
+    0 <= v.r &&
+    v.r < SIZE &&
+    0 <= v.c &&
+    v.c < SIZE
   );
 }
 
 Vector add(Vector v1, Vector v2) {
   return (Vector){
-    .r = v1.r + v2.r,
-    .c = v1.c + v2.c,
+    v1.r + v2.r,
+    v1.c + v2.c,
   };
 }
 
-void get_diagonal(Grid grid, Vector start, Vector direction) {
-  Vector curr = start;
-  while (in_bounds(grid, curr.r, curr.c)) {
-    printf("%c\n", get_grid_item(grid, curr.r, curr.c));
-    curr = add(curr, direction);
-  }
+Vector weighted_sum(int s1, Vector v1, int s2, Vector v2) {
+  return (Vector){
+    s1 * v1.r + s2 * v2.r,
+    s1 * v1.c + s2 * v2.c,
+  };
 }
 
-// "Simple occurrences" = horizontal and forwards
-int count_simple_occurrences(Grid grid) {
-  int positions = grid.col_count - PATTERN_SIZE + 1;
-  int result = 0;
-  for (int r = 0; r < grid.row_count; r++) {
-    for (int c = 0; c < positions; c++) {
-      if (
-        get_grid_item(grid, r, c) == PATTERN[0] &&
-        get_grid_item(grid, r, c + 1) == PATTERN[1] &&
-        get_grid_item(grid, r, c + 2) == PATTERN[2] &&
-        get_grid_item(grid, r, c + 3) == PATTERN[3]
-      ) {
-        result++;
-      }
+bool is_match(Vector pos, Vector direction) {
+  for (int i = 0; i < PATTERN_SIZE; i++) {
+    char expected = PATTERN[i];
+    char actual = get_input_char(weighted_sum(1, pos, i, direction));
+    if (expected != actual) {
+      return false;
     }
+  }
+  return true;
+}
+
+int count_matches(Vector start, Vector direction) {
+  int result = 0;
+  for (
+    Vector pos = start;
+    in_bounds(weighted_sum(1, pos, PATTERN_SIZE - 1, direction));
+    pos = add(pos, direction)
+  ) {
+    result += is_match(pos, direction);
   }
   return result;
 }
 
-void show_grid(Grid grid) {
-  for (int r = 0; r < grid.row_count; r++) {
-    for (int c = 0; c < grid.col_count; c++) {
-      printf("%c ", get_grid_item(grid, r, c));
-    }
-    puts("");
-  }
-}
-
 int main() {
-  /* FILE *file = fopen("ex", "r"); */
-  /* FILE *file = fopen("in", "r"); */
+  int answer = 0;
 
-  /* int* arr = (int*) malloc(10 * sizeof(int)); */
-  /* arr[0] = 1; */
-  /* print_int(arr[0]); */
+  // Top
+  for (Vector pos = { 0,        0,        }; pos.c < SIZE; pos.c++) answer += count_matches(pos, DOWN);
 
-  /* int answer = 0; */
-  /* printf("%d\n", answer); */
+  for (Vector pos = { 0,        0,        }; pos.c < SIZE; pos.c++) answer += count_matches(pos, SOUTHWEST);
+  for (Vector pos = { 1,        SIZE - 1, }; pos.r < SIZE; pos.r++) answer += count_matches(pos, SOUTHWEST);
 
-  show_grid(example_grid);
+  // Right side
+  for (Vector pos = { 0,        SIZE - 1, }; pos.r < SIZE; pos.r++) answer += count_matches(pos, LEFT);
 
-  Vector origin = { .r = 0, .c = 0 };
-  get_diagonal(example_grid, origin, SOUTHEAST);
+  for (Vector pos = { 0,        SIZE - 1, }; pos.r < SIZE; pos.r++) answer += count_matches(pos, NORTHWEST);
+  for (Vector pos = { SIZE - 2, SIZE - 1, }; pos.c >= 0;   pos.c--) answer += count_matches(pos, NORTHWEST);
 
-  // top down
-  // bottom up
-  // leftside rightward
-  // rightside leftward
-  // L northwest
-  // L northeast
-  // L southwest
-  // L southeast
+  // Bottom
+  for (Vector pos = { SIZE - 1, SIZE - 1, }; pos.c >= 0;   pos.c--) answer += count_matches(pos, UP);
+
+  for (Vector pos = { SIZE - 1, SIZE - 1, }; pos.c >= 0;   pos.c--) answer += count_matches(pos, NORTHEAST);
+  for (Vector pos = { SIZE - 2, 0,        }; pos.r >= 0;   pos.r--) answer += count_matches(pos, NORTHEAST);
+
+  // Left side
+  for (Vector pos = { SIZE - 1, 0,        }; pos.r >= 0;   pos.r--) answer += count_matches(pos, RIGHT);
+
+  for (Vector pos = { SIZE - 1, 0,        }; pos.r >= 0;   pos.r--) answer += count_matches(pos, SOUTHEAST);
+  for (Vector pos = { 1,        0,        }; pos.c < SIZE; pos.c++) answer += count_matches(pos, SOUTHEAST);
+
+  printf("%d\n", answer);
 }
