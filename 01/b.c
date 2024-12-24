0379358bde1c707aa163bdaf900ca746d14e59e5
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2024-12-24 12:06:31 -0800

    01b cleanup: Use argv

--- 01/b.c
+++ 01/b.c
@@ -3,8 +3,8 @@
 
 const int MAX_SIZE = 2000;
 
-void read_input_file(int* arr1, int* arr2, int* size) {
-  FILE *file = fopen("in", "r");
+void read_input_file(char* filename, int* arr1, int* arr2, int* size) {
+  FILE *file = fopen(filename, "r");
   int elem1, elem2;
   int i = 0;
   while (fscanf(file, "%d %d", &elem1, &elem2) != EOF) {
@@ -30,11 +30,17 @@ int count_occurrences(int n, int* arr, int size) {
 }
 
 // TODO Maybe do the faster (linear time?) solution
-int main() {
+int main(int argc, char** argv) {
+  if (argc < 2) {
+    fprintf(stderr, "Usage:\n");
+    fprintf(stderr, " ./a ex\n");
+    fprintf(stderr, " ./a in\n");
+    exit(EXIT_FAILURE);
+  }
   int left[MAX_SIZE];
   int right[MAX_SIZE];
   int size;
-  read_input_file(left, right, &size);
+  read_input_file(argv[1], left, right, &size);
 
   int answer = 0;
   for (int i = 0; i < size; i++) {
